9906c01a18a215da1f975ea26881855f90d4c4b3
Adam Katora <adamkatora@gmail.com>
2024-03-25 14:55:55 -0400

    Add auth routes

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,14 @@
 import router from '@adonisjs/core/services/router'
 router.on('/inertia').renderInertia('home', { version: 6 })
 
+/**
+ * Auth routes
+ */
+const AuthController = () => import('#controllers/auth_controller')
+
+router
+  .group(() => {
+    router.get('/sign_in', [AuthController, 'showSignIn'])
+    router.post('/sign_in', [AuthController, 'signIn'])
+  })
+  .prefix('/auth')
