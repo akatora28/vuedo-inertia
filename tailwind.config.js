b295954453cdde1edac6e25f9aa67e3ab0ecee05
Adam Katora <adamkatora@gmail.com>
2024-03-25 14:26:32 -0400

    Switch to class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'selector',
+  darkMode: 'class',
   content: ['./resources/**/*.edge', './inertia/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
     extend: {},
