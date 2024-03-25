d3d60766c5d3ae4db7fd3ac44e9fae1aa5c562f0
Adam Katora <adamkatora@gmail.com>
2024-03-25 17:02:48 -0400

    Update auth_controller.ts

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,4 +1,29 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+
+import User from '#models/user'
+import { signInValidator } from '#validators/auth/sign_in'
 
 export default class AuthController {
+  // This is GET /sign_in
+  async showSignIn({ inertia }: HttpContext) {
+    return inertia.render('auth/sign_in')
+  }
+
+  // This is POST /sign_in
+  async signIn({ auth, request, response, session }: HttpContext) {
+    const { email, password } = await request.validateUsing(signInValidator)
+
+    try {
+      const user = await User.verifyCredentials(email, password)
+      await auth.use('web').login(user)
+
+      // Dashboard hasn't been created yet
+      return response.redirect().toPath('/dashboard')
+    } catch {
+      // TODO - could add more complex error handling?
+      session.flash('errors', { email: ['Invalid credentials'], password: ['Invalid credentials'] })
+
+      return response.redirect().back()
+    }
+  }
 }
