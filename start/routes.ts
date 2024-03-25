97c35c785e4b26f9dc05b81e90624cdbceac5b66
Adam Katora <adamkatora@gmail.com>
2024-03-25 17:21:30 -0400

    Add dashboard route and auth middleware

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,8 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+
 router.on('/inertia').renderInertia('home', { version: 6 })
 
 /**
@@ -21,3 +23,14 @@ router
     router.post('/sign_in', [AuthController, 'signIn'])
   })
   .prefix('/auth')
+
+/**
+ * Dashboard route(s?)
+ */
+const DashboardController = () => import('#controllers/dashboard_controller')
+
+router
+  .group(() => {
+    router.get('/', [DashboardController, 'show'])
+  })
+  .use(middleware.auth())
