1d1815809b376180a24dbfd2b19e3430bad2a804
Adam Katora <adamkatora@gmail.com>
2024-03-26 10:22:09 -0400

    Scaffold header / general layout

--- inertia/layouts/default/dashboard_layout.vue
+++ inertia/layouts/default/dashboard_layout.vue
@@ -3,8 +3,17 @@
 </script>
 <template>
   <div class="flex h-screen overflow-hidden">
-    <h1>Dashboard Layout</h1>
+
     <!-- Sidebar? -->
-    <slot />
+
+    <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      <header class="sticky top-0 z-50 flex w-full bg-white dark:bg-black">
+        <div class="flex flex-grow items-center justify-end px-4 py-4">
+          <h1 class="text-black dark:text-white">Dashboard Layout</h1>
+        </div>
+
+      </header>
+      <slot />
+    </div>
   </div>
 </template>
